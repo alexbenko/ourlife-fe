026275ca7459c8be2d4b6f0406fcebbc99573129
Alex Benko <benkoalex521@gmail.com>
2021-06-26 16:59:13 -0400

    Base app set up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,43 @@
+import { useEffect , useState } from 'react'
 import Head from 'next/head'
+import useSWR from 'swr'
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import styles from '../styles/Home.module.css'
+import AlbumCard from '../components/AlbumCard'
+
 
 export default function Home() {
+  const [ albumsInfo, setAlbumsInfo ] = useState([])
+
+  const fetcher = (url : string) => fetch(url).then(r => r.json())
+  const { data, error } = useSWR('http://localhost:8080/api/albums/all', fetcher)
+  // TODO: When deploying to production, create an environemnt variable in vercel that will store the url for the other server
+  if (data) console.log(data)
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Our Life</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Our Life
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Fry, we have a crate to deliver. The alien mothership is in orbit here. If we can hit that bullseye, the rest of the dominoes will fall like a house of cards. Checkmate. Oh, how I wish I could believe or understand that! There's only one reasonable course of action now: kill Flexo!
+          (This is where i will put a little description describing the site.)
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {
+          !data ? <CircularProgress /> :
+          <div className={styles.grid}>
+            {data.map((album,i) => <AlbumCard albumName={album.album_name} id={album.id} key={i}/>)}
+          </div>
+        }
       </main>
 
       <footer className={styles.footer}>
@@ -63,3 +53,10 @@ export default function Home() {
     </div>
   )
 }
+
+/*
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const res = await fetch('http://localhost:8080/api/albums/all') // TODO : When deplying to production changes this to an environment variable for image microservice server
+    const data = await res.json()
+}
+*/
