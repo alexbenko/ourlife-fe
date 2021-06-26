716fd5c9d0fb4b6ca33b18532e670f2287c086a5
Alex Benko <benkoalex521@gmail.com>
2021-06-26 16:58:41 -0400

    Removed log

--- components/AlbumCard.tsx
+++ components/AlbumCard.tsx
@@ -1,7 +1,6 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 export default function AlbumCard({ albumName, id }) {
-  console.log(id)
   return (
     <div className={styles.card}>
       <Link href={`/album/${id}`}>
