4034d7cde1cf52bf301849096baab14da17a66f5
Alex Benko <benkoalex521@gmail.com>
2021-07-02 09:25:07 -0400

    imported favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <Component {...pageProps} />
     </>
