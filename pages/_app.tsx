44cb7a6405faeb7625e15f0bbb02b51a6bfdef5c
Alex Benko <benkoalex521@gmail.com>
2021-07-02 12:46:52 -0400

    temp hot fix until i set up ssl on my microservice

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import '../styles/globals.css'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
+  // REMOVE THE CONTENT SECURITY POLICY once i add ssl to my microservice
   return (
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <Component {...pageProps} />
