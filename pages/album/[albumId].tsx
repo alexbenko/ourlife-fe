f46a504a21993c35ecfc419ba4e5301814c05a51
Alex Benko <benkoalex521@gmail.com>
2021-07-10 11:10:34 -0400

    Uses url for config now

--- pages/album/[albumId].tsx
+++ pages/album/[albumId].tsx
@@ -2,11 +2,12 @@ import { InferGetStaticPropsType } from 'next'
 import Link from 'next/link'
 import styles from '../../styles/Home.module.css'
 import HomeIcon from '@material-ui/icons/Home'
+import url from '../../config/url'
 
-// TODO: create a config that will export either the url of the localhost if in dev or the server url if in production
-// https://stackoverflow.com/questions/64309158/nextjs-env-variable-is-always-undefined
+const API_URL = url()
+
 export const getStaticProps = async ({ params }) => {
-  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + `api/albums/${params.albumId}`)
+  const res = await fetch(API_URL + `api/albums/${params.albumId}`)
   const data = await res.json()
 
   return {
@@ -18,7 +19,7 @@ export const getStaticProps = async ({ params }) => {
 }
 
 export async function getStaticPaths() {
-  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + 'api/albums/all')
+  const res = await fetch(API_URL + 'api/albums/all')
   const albums = await res.json()
 
   // Get the paths we want to pre-render based on posts
@@ -39,11 +40,9 @@ const Album = ({ data }) => {
       <Link href='/'>
         <a> <HomeIcon /> </a>
       </Link>
-      <div className={styles.container}>
        <div className={styles.grid}>
-         {data.map((image) => <img style={{maxWidth:'20%', maxHeight: '20%'}} src={process.env.NEXT_PUBLIC_API_URL + '/photos/' + image.img_url}></img>)}
+         {data.map((image) => <img style={{maxWidth:'20%', maxHeight: '20%'}} src={API_URL + 'photos' + image.img_url}></img>)}
        </div>
-      </div>
     </>
   )
 }
