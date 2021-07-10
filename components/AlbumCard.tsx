fe12be760de6f2002f9f5dde1bfbc4001c0aede7
Alex Benko <benkoalex521@gmail.com>
2021-07-10 11:13:59 -0400

    Whole Component is wrapped in link

--- components/AlbumCard.tsx
+++ components/AlbumCard.tsx
@@ -2,12 +2,12 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 export default function AlbumCard({ albumName, id }) {
   return (
-    <div className={styles.card}>
-      <Link href={`/album/${id}`}>
-        <a>
+    <Link href={`/album/${id}`}>
+      <a>
+        <div className={styles.card}>
           <h3> {albumName} &rarr;</h3>
-        </a>
-      </Link>
-    </div>
+      </div>
+      </a>
+    </Link>
   )
 }
