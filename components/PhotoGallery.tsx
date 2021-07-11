04a7a769de9d209d1ba47facf152bcca06cb8bcc
Alex Benko <benkoalex521@gmail.com>
2021-07-11 09:49:53 -0400

    Component to display images in a columns

---
+++ components/PhotoGallery.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Gallery.module.css'
+
+export default function PhotoGallery({ columns, API_URL }){
+  return(
+    <div className={styles.gallery}>
+      {columns.map(column => {
+        return(
+          <div className={styles.column}>
+            {column.map(img => <img className={styles.column_img} src={API_URL + 'photos' + img.img_url}/>)}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
