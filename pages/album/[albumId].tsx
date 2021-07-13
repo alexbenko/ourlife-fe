a1670e4a64c9f4247cfd51d778aa341c9bb34272
Alex Benko <benkoalex521@gmail.com>
2021-07-13 09:35:56 -0400

    Data is formatted in getStaticProps now

--- pages/album/[albumId].tsx
+++ pages/album/[albumId].tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import HomeIcon from '@material-ui/icons/Home'
 import url from '../../config/url'
@@ -7,9 +6,22 @@ import styles from '../../styles/Home.module.css'
 import PhotoGallery from '../../components/PhotoGallery'
 
 export const getStaticProps = async ({ params }) => {
+  const formatData = (imageData : [{}]) =>{
+    // i want an array with 4 sub arrays of similar length
+    const amountPerColumn = Math.floor(imageData.length / 4)
+    let formatted = []
+    while(imageData.length > 0) {
+      let chunk = imageData.splice(0, amountPerColumn)
+      formatted.push(chunk)
+    }
+
+    return formatted
+  }
+
   const API_URL = url()
   const res = await fetch(API_URL + `api/albums/${params.albumId}`)
-  const data = await res.json()
+  let data = await res.json()
+  data = formatData(data)
   // TODO: Add a join on the backend so it will pull the album info as well as all the image paths for that
   return {
     props: {
@@ -37,31 +49,13 @@ export async function getStaticPaths() {
 
 
 const Album = ({ data, API_URL }) => {
-  const formatData = (imageData : [{}]) =>{
-    let formatted = []
-    while(imageData.length > 0) {
-      let chunk = imageData.splice(0,4)
-      formatted.push(chunk)
-    }
-
-    return formatted
-  }
-
-  const [formattedData, setFormattedData] = useState([])
-
-  useEffect(()=>{
-    setFormattedData(formatData(data))
-  },[])
   return(
     <>
       <Link href='/'>
         <a> <HomeIcon /> </a>
       </Link>
       <div className={styles.container}>
-
-        <h3>Hello</h3>
-
-        {formattedData.length ? <PhotoGallery columns={formattedData} API_URL={API_URL}/> : null}
+        {data.length ? <PhotoGallery columns={data} API_URL={API_URL}/> : null}
       </div>
     </>
   )
