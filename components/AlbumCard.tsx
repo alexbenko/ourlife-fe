8b1ca049a4b0d092d36927b6e3dc4e87347f8f65
Alex Benko <benkoalex521@gmail.com>
2021-07-30 22:30:30 -0400

    Conditional rendering

--- components/AlbumCard.tsx
+++ components/AlbumCard.tsx
@@ -1,13 +1,17 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 export default function AlbumCard({ albumName, id }) {
-  return (
-    <Link href={`/album/${id}`}>
-      <a>
-        <div className={styles.card}>
-          <h3> {albumName} &rarr;</h3>
-        </div>
-      </a>
-    </Link>
-  )
+  if (albumName) {
+    return (
+      <Link href={`/album/${id}`}>
+        <a>
+          <div className={styles.card}>
+            <h3> {albumName} &rarr;</h3>
+          </div>
+        </a>
+      </Link>
+    )
+  } else {
+    return null
+  }
 }
