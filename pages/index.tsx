97afa19b8fb5781692a0b059b49b39c5f3834fee
Alex Benko <benkoalex521@gmail.com>
2021-08-04 10:30:05 -0400

    alerts error when fetching index data in dev mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import useSWR from 'swr'
-import CircularProgress from '@material-ui/core/CircularProgress';
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 import styles from '../styles/Home.module.css'
 import AlbumCard from '../components/AlbumCard'
@@ -7,6 +7,9 @@ export default function Home({ API_URL }) {
   const fetcher = (url : string) => fetch(url).then(r => r.json())
   const { data, error } = useSWR(API_URL + '/api/albums/all', fetcher)
 
+  if(error && process.env.NODE_ENV === 'development') {
+    alert(error)
+  }
   return (
     <div className={styles.container}>
 
