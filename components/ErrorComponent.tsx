78b9f2db72f4facc037066178f7d04783598030a
Alex Benko <benkoalex521@gmail.com>
2021-08-26 21:42:38 -0500

    Will conditionally redirect to home

--- components/ErrorComponent.tsx
+++ components/ErrorComponent.tsx
@@ -4,21 +4,35 @@ import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
 export default function ErrorComponent ({ errorMsg, redirect }) {
-  if(redirect) {
-    const [ seconds, setSeconds ] = useState(5)
-    useEffect(()=>{
+  const [ seconds, setSeconds ] = useState(5)
+  const router = useRouter()
 
+  if (redirect) {
+    useEffect(()=>{
+      const timeout = setInterval(()=>{
+        setSeconds(seconds => seconds - 1)
+      }, 1000) // every second
+      return ()=> clearInterval(timeout)
     }, [])
+
+    useEffect(() => {
+      if(seconds === 0) {
+        router.push('/')
+      }
+    }, [seconds])
   }
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-          <h1 className={styles.title}> Invalid Request </h1>
-          <p className={styles.description}>
-            Reason: {errorMsg}
-          </p>
-          <p className={styles.description}> Please contact the admin for help.</p>
-      </main>
+      <h1 className={styles.title}> Invalid Request </h1>
+      <p className={styles.description}>
+        Reason: {errorMsg}
+      </p>
+      <p className={styles.description}> Please contact the admin for help.</p>
+      { redirect &&
+        <p>
+          Redirecting in {seconds}
+        </p>
+      }
     </div>
   )
 }
