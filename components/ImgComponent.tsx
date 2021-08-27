611823c4402a290f7fdf69390e5dc9df41a0101d
Alex Benko <benkoalex521@gmail.com>
2021-08-26 21:00:44 -0500

    Custom img component

---
+++ components/ImgComponent.tsx
@@ -0,0 +1,16 @@
+import { useState,useEffect } from 'react'
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+export default function ImgComponent({ srcUrl, className, keyNum}){
+  const [ loaded, setLoaded ] = useState(false)
+  const render = () => {
+    console.log('Loaded: ', keyNum)
+    setLoaded(true)
+  }
+    return(
+      <>
+        <img key={keyNum} loading='lazy' className={className}  onLoad={()=> render()} src={srcUrl}/>
+        {!loaded && <CircularProgress />}
+      </>
+    )
+}
