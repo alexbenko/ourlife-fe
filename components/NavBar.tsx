3a29a6b92c053bb507b57bdabc5cd44a49d0a6a5
Alex Benko <benkoalex521@gmail.com>
2021-09-06 18:18:46 -0500

    navigation for non mobile

---
+++ components/NavBar.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import styles from '../styles/navbar.module.css'
+import { useRouter } from 'next/router'
+
+
+export default function NavBar({ navLinks }) {
+  const router = useRouter()
+
+  return(
+    <div className={styles.nav_container}>
+      <Link href="/">
+        <a>
+          <p className={styles.title}> The Wanderers {router.query?.albumId && 'In'} </p>
+        </a>
+      </Link>
+        {navLinks.map((link, i) => {
+          return (
+            <Link href={link.url} key={i}>
+              <a> {link.icon} {link.text} </a>
+            </Link>
+          )
+        })}
+
+    </div>
+  )
+}
