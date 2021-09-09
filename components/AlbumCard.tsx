3d20b7b04d444dd111802668c5a900f0eaf611fd
Alex Benko <benkoalex521@gmail.com>
2021-09-09 15:51:12 -0500

    Remove unneeded import

--- components/AlbumCard.tsx
+++ components/AlbumCard.tsx
@@ -1,6 +1,6 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import { useState } from 'react'
+
 export default function AlbumCard({ albumName, id }) {
   if (albumName) {
     return (
