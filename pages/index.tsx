11c60e0af69362efc1b6cf19bc52f99498853309
Alex Benko <benkoalex521@gmail.com>
2021-09-09 15:52:48 -0500

    Renders 3 random advenure links with thumbnail images and added error handling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,46 @@
 import styles from '../styles/Home.module.css'
+import url from '../config/url'
+import ErrorComponent from '../components/ErrorComponent'
+import ImgWithText from '../components/ImgWithText'
+import Link from 'next/link'
 
-export default function Home({ API_URL }) {
+
+export async function getServerSideProps(context) {
+  const API_URL = url()
+  const req = await fetch(API_URL + '/api/albums/fe/index')
+  const res = await req.json()
+
+  if(!res.success) {
+    return {
+      props: { success: false, msg: res.error}
+    }
+  }
+  return {
+    props: { data: res.data, success: true}
+  }
+}
+
+export default function Home(props) {
+  if(!props.success) {
+    return <ErrorComponent errorMsg={props.msg } redirect={false} helpMsg={false}/>
+  }
+
   return (
     <div className={styles.container}>
-      <div>
-      (Site Under Development)
-
-Random adventure which is link to the album
-
-Text explaining where the pictures are from and why we like to travel etc etc
-
-stats on where we have traveled
-      </div>
-
-
+      <main className={styles.main}>
+          <h2 style={{textAlign: 'center'}}> Adventures </h2>
+          <div className={styles.grid}>
+            {props.data.map((adv, i) => {
+              return(
+                <Link href={`/adventure/${adv.id}`}>
+                  <a>
+                    <ImgWithText src={`${props.API_URL}/photos${adv.thumbnail}`} text={adv.displayname}/>
+                  </a>
+                </Link>
+              )
+            })}
+          </div>
+      </main>
     </div>
   )
 }
