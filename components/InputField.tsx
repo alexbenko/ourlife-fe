40e6ddd05fd8b1b9261d9f78d689264552dd9529
Alex Benko <benkoalex521@gmail.com>
2021-09-26 13:52:10 -0500

    Minor changes

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,13 +1,15 @@
 import styles from '../styles/inputfield.module.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function InputField({ label, onChange, value, error }){
   const [ active, setActive] = useState(false)
+
   return(
     <div className={styles.inputContainer}>
       <input  type='text' value={value} onChange={(e)=>{
           onChange(e)
-          if(value !== ''){
+          console.log(`${label} Value: `, value)
+          if(value.length){
             setActive(true)
           } else {
             setActive(false)
