bec02a669174ae7012c5a38e4370a7c8ba2e550c
Alex Benko <benkoalex521@gmail.com>
2021-10-14 15:55:25 -0500

    removed state and style is based on props

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,22 +1,10 @@
 import styles from '../styles/inputfield.module.css'
-import { useState, useEffect } from 'react'
 
-export default function InputField({ label, onChange, value, error }){
-  const [ active, setActive] = useState(false)
-
+export default function InputField({ label, onChange, value, error, field }){
   return(
     <div className={styles.inputContainer}>
-      <input  type='text' value={value} onChange={(e)=>{
-          onChange(e)
-          console.log(`${label} Value: `, value)
-          if(value.length){
-            setActive(true)
-          } else {
-            setActive(false)
-          }
-        }}
-      />
-      <label style={{transform: active ? 'translate(0, 12px) scale(0.75)' : undefined}} htmlFor="text">
+      <input  type='text' value={value} onChange={(e)=> onChange(e, field)}/>
+      <label style={{transform: value !== '' ? 'translate(0, 12px) scale(0.75)' : undefined}} htmlFor="text">
         { label }
       </label>
     </div>
