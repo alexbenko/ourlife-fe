80ed316b65a59b20c9ca68f81dbeb6f8df5f7d61
Alex Benko <benkoalex521@gmail.com>
2021-10-14 15:54:47 -0500

    refractored tests

--- components/PasswordCreation.tsx
+++ components/PasswordCreation.tsx
@@ -5,56 +5,43 @@ import CloseIcon from '@material-ui/icons/Close'
 import CheckIcon from '@material-ui/icons/Check'
 
 const PasswordCreation = ({ value, setValue }) => {
-  const [ validation, setValidation ] = useState({
-    length: false,
-    oneUppercase: false,
-    oneLowercase: false,
-    oneDigit: false,
-    oneSymbol: false
-  })
-
-  const validateInput = {
-    length: (input) => input.length >= 8,
-    oneUppercase: (input) => /[A-Z]/.test(input),
-    oneLowercase: (input) => /[a-z]/.test(input),
-    oneDigit: (input) => /\d/.test(input),
-    oneSymbol: (input) => /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(input)
-  }
-
-  const helperText = {
-    length: 'Must have at least 8 characters.',
-    oneUppercase: 'Must have at least one uppercase letter.',
-    oneLowercase: 'Must have at least one lowercase letter.',
-    oneDigit: 'Must have at least one digit.',
-    oneSymbol: 'Must have at least one symbol (#?!@$%^&*-).'
-  }
-
-  const changeHandler = (event) =>{
-    event.preventDefault()
-    // setValue(event.target.value)
-    let copyValidation = { ...validation }
-    for(const key in validateInput) {
-      copyValidation[key] = validateInput[key](value)
+  const validation = {
+    length: {
+      text : 'Must have at least 8 characters.',
+      valid: () => value.length >= 8
+    },
+    upperCase: {
+      text: 'Must have at least one uppercase letter.',
+      valid: () => /[A-Z]/.test(value)
+    },
+    lowerCase: {
+      text: 'Must have at least one lowercase letter.',
+      valid: () => /[a-z]/.test(value)
+    },
+    digit: {
+      text: 'Must have at least one digit.',
+      valid: () => /\d/.test(value)
+    },
+    symbol : {
+      text: 'Must have at least one symbol (#?!@$%^&*-).',
+      valid:() => /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(value)
     }
-    setValidation(copyValidation)
   }
+
   return (
     <>
       <InputField
         value={value}
-        onChange={(event) => {
-          console.log('Password change')
-          setValue(event.target.value)
-          console.log('set value')
-          changeHandler(event)
-        }}
+        onChange={(event) => setValue(event.target.value)}
         label='Password'
         error={validation}
       />
       <div>
         {Object.keys(validation).map((key, i) =>{
           return(
-          <p key={i}> {validation[key] ? <CheckIcon style={{ color: 'green' }}/> : <CloseIcon style={{ color: 'red' }}/>} {helperText[key]} </p>
+          <p key={i}> {validation[key].valid() ? <CheckIcon style={{ color: 'green' }}/> : <CloseIcon style={{ color: 'red' }}/>}
+            {validation[key].text}
+          </p>
           )
         })}
       </div>
